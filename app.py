9b9f0882f5d5c4b37e73683d4db179bc642434e1
Joao Righetto <joao.righetto@gmail.com>
2024-08-19 11:34:23 -0300

    feat: initialize Flask app with SQLAlchemy and dotenv support
    - Set up a basic Flask application structure.
    
    - Integrated Flask-SQLAlchemy for ORM support.
    
    - Utilized python-dotenv for environment variable management.
    
    - Configured .gitignore to exclude sensitive and unnecessary files from version control, including .env, SQLite databases, and development tools.
    
    - Defined project dependencies in requirements.txt for Flask, Flask-SQLAlchemy, and python-dotenv.

--- app.py
+++ app.py
@@ -1,7 +1,25 @@
+import os
+
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+
+
+load_dotenv()
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+
+db = SQLAlchemy(app)
+
 
 @app.route('/')
 def home():
     return "Hello, World!"
+
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
