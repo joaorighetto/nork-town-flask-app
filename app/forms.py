f95f17095cea3b8493d923dc9e7d165c5e188ca3
Joao Righetto <joao.righetto@gmail.com>
2024-08-21 08:32:20 -0300

    fix: set default is_admin property value to False

--- app/forms.py
+++ app/forms.py
@@ -3,11 +3,11 @@ from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Length
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired(), Length(min=4, max=15)])
-    password = PasswordField('Password', validators=[DataRequired(), Length(min=6, max=15)])
+    username = StringField('Username', validators=[DataRequired(), Length(min=3, max=15)])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=3, max=15)])
     submit = SubmitField('Register')
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired(), Length(min=4, max=15)])
-    password = PasswordField('Password', validators=[DataRequired(), Length(min=6, max=15)])
+    username = StringField('Username', validators=[DataRequired(), Length(min=3, max=15)])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=3, max=15)])
     submit = SubmitField('Login')
--- app/models/user.py
+++ app/models/user.py
@@ -8,7 +8,7 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(15), unique=True, nullable=False)
     password_hash = db.Column(db.String(15), nullable=False)
-    is_admin = db.Column(db.Boolean, default=True)
+    is_admin = db.Column(db.Boolean, default=False)
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
