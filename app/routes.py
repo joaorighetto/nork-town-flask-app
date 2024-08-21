c92f057afc07d72106ebc9df13dd7cf4b489f43a
Joao Righetto <joao.righetto@gmail.com>
2024-08-21 10:02:22 -0300

    fix: remove unnecessary import

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, redirect, url_for, flash
+from flask import render_template, redirect, url_for
 from flask_login import login_user, logout_user, login_required, current_user
 
 from app.forms import RegistrationForm, LoginForm
